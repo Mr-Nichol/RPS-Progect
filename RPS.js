4eb938b85c7ef35b5e02d44ea90e4395a75030fb
Nichol Fernando <115018248+Mr-Nichol@users.noreply.github>
2023-05-10 16:19:43 +0200

    change functions in RPS.JS & add event listners

--- RPS.js
+++ RPS.js
@@ -1,40 +1,44 @@
 const array = ["rock" , "paper" , "scissors"];
+const btns = document.querySelectorAll('input');
+const marks = document.querySelector('.score')
+const stats = document.querySelector('.status')
+
+function disableButtons(){
+    btns.forEach(function(rbtn){
+        rbtn.disabled = true;        
+    })
+}
+
 function getComputerChoice(arr){
     const randomIndex = Math.floor(Math.random()*arr.length);
     const computersChoice = arr[randomIndex];  
     return computersChoice;  
 }
 
-function getPlayersChoice(){
-    const playersChoice = prompt("Rock,Paper or scissors?").toLowerCase();
-    return playersChoice;
-
-}
-
-
 let score = 0;
-function playround(){
+function playround(playersSelection){
     const computersSelection = getComputerChoice(array);
-    const playersSelection = getPlayersChoice();
-    // console.log(computersSelection);
-    // console.log(playersSelection);
     if(computersSelection === playersSelection){
-        score = ++score;
-        console.log("you won next round");
-
+        score += 1;
+        if (score>=5){
+            marks.textContent = "you won";
+            disableButtons()
+            
+        }
+        else(score<5);
+            stats.textContent = "your score is " + score;
+            marks.textContent = "you won next round";
     }
     else {
-            console.log(" try agin");
-            }       
+        marks.textContent = "try agin";
+        console.log("try agin");
+            }            
     }
 
-function game(){
-    // console.log(playround()=== false);
-    for(i=0;i<5;i++){
-            playround();
-        }
-    console.log("your score is " + score);
 
-    }
-
-game();
+    
+btns.forEach(btn =>{
+    btn.addEventListener('click',function (){
+        playround(btn.value);
+    });
+});   
